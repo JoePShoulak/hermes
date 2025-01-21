1d5a01f76a73c4834b40e860ad87aef1c838290b
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-01-21 00:36:37 -0600

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import PowerStatus from "./PowerStatus";
 
 function App() {
   return (
-    <div className="App">
-      <PowerStatus />
+    <div>
+      <h1>Welcome to Vite + React</h1>
     </div>
   );
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:5000", // Ensure this matches your backend address
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
