f0a5dea80d52c66bd6f7e5fff6c909b3741c5736
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-01-21 19:30:17 -0600

    update

--- client/src/Status1.jsx
+++ client/src/Status1.jsx
@@ -1,9 +1,26 @@
 import React, { useEffect, useState } from "react";
 
+const defaultStatus = {
+  powered: null,
+};
+
 export default function Status1(props) {
+  const [status, setStatus] = useState(defaultStatus);
+  function updateStatus(key, value) {
+    setStatus(prevState => ({
+      ...prevState,
+      [key]: value,
+    }));
+  }
+
   return (
     <div>
-      <p>My name is {props.target}</p>
+      <p>
+        <b>{props.target}</b>
+      </p>
+      <ul>
+        <li>Powered: {status.powered}</li>
+      </ul>
     </div>
   );
 }
