5c99530242c7b5e49dbdacd8c378733dd1a5e610
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-01-22 01:12:40 -0600

    update

--- monitor.py
+++ monitor.py
@@ -93,7 +93,6 @@ def get_status(target):
 	
     status["power"] = True if status["online"] else get_power(target)
     status["uid"] = False if not status["power"] else get_UID(target)
-    print(status["uid"])
     
     return status
 
