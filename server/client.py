c9f2bda6a048cd46f23101ac13e32775fa023af8
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-01-22 16:26:12 -0600

    update

--- server/client.py
+++ server/client.py
@@ -1,6 +1,17 @@
 import argparse
 from commands import *
-from monitor import *
+
+HPs = ["hp1", "hp2", "hp3", "hp4"]
+
+def prettify_status(data):
+    result = []
+    for host, status in data.items():
+        result.append(f'\nHost: {host.upper()}')
+        result.append(f'  - State: {status["state"]}')
+        result.append(f'  - Docker: {status["docker"]}')
+        result.append(f'  - UID Light: {status["uid"]}')
+        result.append(f'  - Uptime: {status["uptime"]}')
+    return "\n".join(result)
 
 if __name__ == "__main__":
     print("UPS Status:", get_ups())
--- server/routes/api/index.js
+++
@@ -1,5 +0,0 @@
-const router = require("express").Router();
-const powerRoutes = require("./power");
-
-router.use("/power", powerRoutes);
-module.exports = router;
