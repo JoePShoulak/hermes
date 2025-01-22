3094bcc1653fd9c1c7b24fcbb423fbac0cf4c328
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-01-22 16:38:46 -0600

    update

--- server/server.py
+++ server/server.py
@@ -24,6 +24,7 @@ def host_status(host):
     if request.method == 'PUT':
         return f'PUT host_status {host}'
     else:
+        get_status(host)
         return f'host_status {host}'
 
 @app.route('/api/docker/<host>', methods=['GET'])
