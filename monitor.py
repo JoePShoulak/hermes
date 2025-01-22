2d6811c21fb3186c30e7fac7abfbdb033765e149
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-01-21 23:30:19 -0600

    update

--- monitor.py
+++ monitor.py
@@ -16,6 +16,11 @@ def execute_command(command, parser=lambda output: output):
         raise CommandExecutionError(error_message, e.returncode)
 
 # GETTERS / SETTERS / PARSERS
+# # GENERAL
+def re_parse(output, query):
+    match = re.search(query, output, re.IGNORECASE)
+    return match.group(1).capitalize() if match else "UNKNOWN"
+
 # # POWER
 def get_power(target):
     return execute_command(f"ilo {target} POWER", parse_power)
@@ -24,12 +29,14 @@ def set_power(target, value):
     return execute_command(f"ilo {target} POWER {value}", parse_power)
     
 def parse_power(output):
-    match = re.search(r"\b(On|Off)\b", output, re.IGNORECASE)
-    return match.group(1).capitalize() if match else "UNKNOWN"
+    return re_parse(output, r"\b(On|Off)\b")
 
 # # Online
 def get_online(target):
     return execute_command(f"ping -C 1 {target}")
+
+def parse_online(output):
+    return re_parse(output, r"\b(0 rec|1 rec)\b")
 
 HPs = ["hp1", "hp2", "hp3", "hp4"]
 
