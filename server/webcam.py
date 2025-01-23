3216d078a889ba5209ec348810c60649fc6c5f7e
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-01-22 22:32:19 -0600

    zach approved

--- server/webcam.py
+++ server/webcam.py
@@ -1,22 +1,26 @@
-from flask import Flask, Response
+from flask import Flask, Response, jsonify
 import cv2
+import json
+from pathlib import Path
+from commands import *
 
-app = Flask(__name__)
+app = Flask('Hermes')
+
+# Initialize the webcam (change index if necessary)
+video_capture = cv2.VideoCapture(0)
 
 def generate_frames():
-    camera = cv2.VideoCapture(0)  # 0 for the first webcam (/dev/video0)
-    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  # Set width
-    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)  # Set height
-
     while True:
-        success, frame = camera.read()
+        # Capture frame-by-frame
+        success, frame = video_capture.read()
         if not success:
             break
         else:
-            # Encode the frame as JPEG
+            # Encode the frame in JPEG format
             _, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
-            # Yield the frame data as part of an HTTP response
+
+            # Yield the frame in byte format
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
@@ -24,5 +28,32 @@ def generate_frames():
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == "__main__":
+def get_data():
+    try:
+        file_path = Path(__file__).with_name('data.json')
+        with file_path.open('r') as fp:
+            return json.load(fp)
+    except:
+        return {}
+
+@app.route('/')
+def index():
+    return "<h1>Webcam Stream</h1><img src='/video_feed'>"
+
+@app.route('/api/host/<host>', methods=['GET'])
+def host_status(host):
+    return jsonify(get_data().get(host))
+
+@app.route('/api/host/<host>/<command>/<value>', methods=['PUT'])
+def host_status_setter(host, command, value):
+    result = execute_command(f"ilo {host} {command.upper()} {value.upper()}")
+    return jsonify({
+        "result": "success" if result != 'UNKNOWN' else 'failure'
+    })
+
+@app.route('/api/ups', methods=['GET'])
+def ups_status():
+    return jsonify(get_data().get('ups'))
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
